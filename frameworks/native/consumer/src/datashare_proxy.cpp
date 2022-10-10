3480b9f12f6591e1f240774a137b4f360900b2fa
niudongyao <niudongyao@huawei.com>
2022-10-10 20:52:50 +0800

    errorcode
    Signed-off-by: niudongyao <niudongyao@huawei.com>

--- frameworks/native/consumer/src/datashare_proxy.cpp
+++ frameworks/native/consumer/src/datashare_proxy.cpp
@@ -354,7 +354,7 @@ int DataShareProxy::BatchInsert(const Uri &uri, const std::vector<DataShareValue
     int32_t err = Remote()->SendRequest(CMD_BATCH_INSERT, data, reply, option);
     if (err != DATA_SHARE_NO_ERROR) {
         LOG_ERROR("GetFileTypes fail to SendRequest. err: %{public}d", err);
-        return err == PERMISSION_ERR ? PERMISSION_ERR_CODE : index;
+        return err == PERMISSION_ERR ? PERMISSION_ERR_CODE : ret;
     }
 
     if (!reply.ReadInt32(ret)) {
