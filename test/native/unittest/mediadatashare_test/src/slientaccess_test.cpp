dacc3e78535d7bbc794e4934e42137e755a91cf8
niudongyao <niudongyao@huawei.com>
2023-01-31 03:24:50 +0000

    add slientaccess tdd
    Signed-off-by: niudongyao <niudongyao@huawei.com>

---
+++ test/native/unittest/mediadatashare_test/src/slientaccess_test.cpp
@@ -0,0 +1,184 @@
+/*
+ * Copyright (C) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>
+#include <unistd.h>
+
+#include "accesstoken_kit.h"
+#include "datashare_helper.h"
+#include "datashare_log.h"
+#include "hap_token_info.h"
+#include "iservice_registry.h"
+#include "system_ability_definition.h"
+#include "token_setproc.h"
+
+namespace OHOS {
+namespace DataShare {
+using namespace testing::ext;
+using namespace OHOS::Security::AccessToken;
+constexpr int STORAGE_MANAGER_MANAGER_ID = 5003;
+std::string DATA_SHARE_URI = "datashare:///com.acts.datasharetest";
+std::string SLIENT_ACCESS_URI = "datashare:///com.acts.datasharetest/entry/DB00/TBL00?Proxy=true";
+std::string TBL_STU_NAME = "name";
+std::string TBL_STU_AGE = "age";
+std::shared_ptr<DataShare::DataShareHelper> g_slientAccessHelper;
+
+class SlientAccessTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+    static void TearDownTestCase(void);
+    void SetUp();
+    void TearDown();
+};
+
+std::shared_ptr<DataShare::DataShareHelper> CreateDataShareHelper(int32_t systemAbilityId, std::string uri)
+{
+    LOG_INFO("CreateDataShareHelper start");
+    auto saManager = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+    if (saManager == nullptr) {
+        LOG_ERROR("GetSystemAbilityManager get samgr failed.");
+        return nullptr;
+    }
+    auto remoteObj = saManager->GetSystemAbility(systemAbilityId);
+    while (remoteObj == nullptr) {
+        LOG_ERROR("GetSystemAbility service failed.");
+        return nullptr;
+    }
+    return DataShare::DataShareHelper::Creator(remoteObj, uri);
+}
+
+void SlientAccessTest::SetUpTestCase(void)
+{
+    LOG_INFO("SetUpTestCase invoked");
+    auto dataShareHelper = CreateDataShareHelper(STORAGE_MANAGER_MANAGER_ID, DATA_SHARE_URI);
+    ASSERT_TRUE(dataShareHelper != nullptr);
+    int sleepTime = 3;
+    sleep(sleepTime);
+
+    HapInfoParams info = {
+        .userID = 100,
+        .bundleName = "ohos.datashareclienttest.demo",
+        .instIndex = 0,
+        .appIDDesc = "ohos.datashareclienttest.demo"
+    };
+    HapPolicyParams policy = {
+        .apl = APL_NORMAL,
+        .domain = "test.domain",
+        .permList = {
+            {
+                .permissionName = "ohos.permission.test",
+                .bundleName = "ohos.datashareclienttest.demo",
+                .grantMode = 1,
+                .availableLevel = APL_NORMAL,
+                .label = "label",
+                .labelId = 1,
+                .description = "ohos.datashareclienttest.demo",
+                .descriptionId = 1
+            }
+        },
+        .permStateList = {
+            {
+                .permissionName = "ohos.permission.test",
+                .isGeneral = true,
+                .resDeviceID = { "local" },
+                .grantStatus = { PermissionState::PERMISSION_GRANTED },
+                .grantFlags = { 1 }
+            }
+        }
+    };
+    AccessTokenKit::AllocHapToken(info, policy);
+    auto testTokenId = Security::AccessToken::AccessTokenKit::GetHapTokenID(
+        info.userID, info.bundleName, info.instIndex);
+    SetSelfTokenID(testTokenId);
+    LOG_INFO("userID = %{public}d, testTokenId = %{public}d.", info.userID, testTokenId);
+
+    g_slientAccessHelper = CreateDataShareHelper(STORAGE_MANAGER_MANAGER_ID, SLIENT_ACCESS_URI);
+    ASSERT_TRUE(g_slientAccessHelper != nullptr);
+    
+    LOG_INFO("SetUpTestCase end");
+}
+
+void SlientAccessTest::TearDownTestCase(void)
+{
+    auto tokenId = AccessTokenKit::GetHapTokenID(100, "ohos.datashareclienttest.demo", 0);
+    AccessTokenKit::DeleteToken(tokenId);
+}
+
+void SlientAccessTest::SetUp(void) {}
+void SlientAccessTest::TearDown(void) {}
+
+HWTEST_F(SlientAccessTest, SlientAccess_Insert_Test_001, TestSize.Level0)
+{
+    LOG_INFO("SlientAccess_Insert_Test_001::Start");
+    auto helper = g_slientAccessHelper;
+    Uri uri(SLIENT_ACCESS_URI);
+    DataShare::DataShareValuesBucket valuesBucket;
+    std::string value = "lisi";
+    valuesBucket.Put(TBL_STU_NAME, value);
+    int age = 25;
+    valuesBucket.Put(TBL_STU_AGE, age);
+
+    int retVal = helper->Insert(uri, valuesBucket);
+    EXPECT_EQ((retVal > 0), true);
+    LOG_INFO("SlientAccess_Insert_Test_001::End");
+}
+
+HWTEST_F(SlientAccessTest, SlientAccess_Update_Test_001, TestSize.Level0)
+{
+    LOG_INFO("SlientAccess_Update_Test_001::Start");
+    auto helper = g_slientAccessHelper;
+    Uri uri(SLIENT_ACCESS_URI);
+    DataShare::DataShareValuesBucket valuesBucket;
+    int value = 50;
+    valuesBucket.Put(TBL_STU_AGE, value);
+    DataShare::DataSharePredicates predicates;
+    std::string selections = TBL_STU_NAME + " = 'lisi'";
+    predicates.SetWhereClause(selections);
+    int retVal = helper->Update(uri, predicates, valuesBucket);
+    EXPECT_EQ((retVal > 0), true);
+    LOG_INFO("SlientAccess_Update_Test_001::End");
+}
+
+HWTEST_F(SlientAccessTest, SlientAccess_Query_Test_001, TestSize.Level0)
+{
+    LOG_INFO("SlientAccess_Query_Test_001::Start");
+    auto helper = g_slientAccessHelper;
+    Uri uri(SLIENT_ACCESS_URI);
+    DataShare::DataSharePredicates predicates;
+    predicates.EqualTo(TBL_STU_NAME, "lisi");
+    vector<string> columns;
+    auto resultSet = helper->Query(uri, predicates, columns);
+    int result = 0;
+    if (resultSet != nullptr) {
+        resultSet->GetRowCount(result);
+    }
+    EXPECT_EQ(result, 1);
+    LOG_INFO("SlientAccess_Query_Test_001::End");
+}
+
+HWTEST_F(SlientAccessTest, SlientAccess_Delete_Test_001, TestSize.Level0)
+{
+    LOG_INFO("SlientAccess_Delete_Test_001::Start");
+    auto helper = g_slientAccessHelper;
+    Uri uri(SLIENT_ACCESS_URI);
+    
+    DataShare::DataSharePredicates deletePredicates;
+    std::string selections = TBL_STU_NAME + " = 'lisi'";
+    deletePredicates.SetWhereClause(selections);
+    int retVal = helper->Delete(uri, deletePredicates);
+    EXPECT_EQ((retVal > 0), true);
+    LOG_INFO("SlientAccess_Delete_Test_001::End");
+}
+} // namespace DataShare
+} // namespace OHOS
