fabffe5f6c9dba4abd139f62f773af933eeb4af5
srr101 <songruirui1@huawei.com>
2023-07-06 14:25:37 +0800

    code check fix
    Signed-off-by: srr101 <songruirui1@huawei.com>

--- frameworks/native/consumer/src/datashare_proxy.cpp
+++ frameworks/native/consumer/src/datashare_proxy.cpp
@@ -170,28 +170,28 @@ int DataShareProxy::Insert(const Uri &uri, const DataShareValuesBucket &value)
 
 int DataShareProxy::Insert(const Uri &uri, const DataShareValuesBucket &value, std::string &result)
 {
-	int index = -1;
-	MessageParcel data;
-	if (!data.WriteInterfaceToken(DataShareProxy::GetDescriptor())) {
-		LOG_ERROR("WriteInterfaceToken failed");
-		return index;
-	}
-	if (!ITypesUtil::Marshal(data, uri, value)) {
-		LOG_ERROR("fail to Marshal value");
-		return index;
-	}
-	MessageParcel reply;
-	MessageOption option;
-	int32_t err = Remote()->SendRequest(CMD_INSERT_EXT, data, reply, option);
-	if (err != DATA_SHARE_NO_ERROR) {
-		LOG_ERROR("Insert fail to SendRequest. err: %{public}d", err);
-		return index;
-	}
-	if (!ITypesUtil::Unmarshal(reply, index, result)) {
-		LOG_ERROR("fail to Unmarshal index");
-		return index;
-	}
-	return index;
+    int index = -1;
+    MessageParcel data;
+    if (!data.WriteInterfaceToken(DataShareProxy::GetDescriptor())) {
+        LOG_ERROR("WriteInterfaceToken failed");
+        return index;
+    }
+    if (!ITypesUtil::Marshal(data, uri, value)) {
+        LOG_ERROR("fail to Marshal value");
+        return index;
+    }
+    MessageParcel reply;
+    MessageOption option;
+    int32_t err = Remote()->SendRequest(CMD_INSERT_EXT, data, reply, option);
+    if (err != DATA_SHARE_NO_ERROR) {
+        LOG_ERROR("Insert fail to SendRequest. err: %{public}d", err);
+        return index;
+    }
+    if (!ITypesUtil::Unmarshal(reply, index, result)) {
+        LOG_ERROR("fail to Unmarshal index");
+        return index;
+    }
+    return index;
 }
 
 int DataShareProxy::Update(const Uri &uri, const DataSharePredicates &predicates, const DataShareValuesBucket &value)
