491d85217d2301dca594464a7f23000adcbc8465
liusaimei <lirui.wx1229126@huawei.com>
2023-08-09 15:08:19 +0800

    add test cases
    Signed-off-by: liusaimei <lirui.wx1229126@huawei.com>

--- test/native/unittest/mediadatashare_test/src/abnormal_branch_test.cpp
+++ test/native/unittest/mediadatashare_test/src/abnormal_branch_test.cpp
@@ -1,6 +1,19 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include <gtest/gtest.h>
 #include <unistd.h>
-
 #include "accesstoken_kit.h"
 #include "datashare_helper.h"
 #include "datashare_log.h"
@@ -10,7 +23,6 @@
 #include "system_ability_definition.h"
 #include "token_setproc.h"
 #include "datashare_errno.h"
-
 #include "published_data_subscriber_manager.h"
 #include "rdb_subscriber_manager.h"
 #include "ishared_result_set_stub.h"
