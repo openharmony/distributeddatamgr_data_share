02636119f088bd736a90ee5087dc1d685b43bd94
wangdengze <wangdengze@huawei.com>
2023-10-25 09:41:10 +0000

    update frameworks/native/proxy/src/ams_mgr_proxy.cpp.
    Signed-off-by: wangdengze <wangdengze@huawei.com>

--- frameworks/native/proxy/src/ams_mgr_proxy.cpp
+++ frameworks/native/proxy/src/ams_mgr_proxy.cpp
@@ -69,7 +69,7 @@ int AmsMgrProxy::Connect(
         LOG_INFO("connect datashareextability start, uri = %{public}s.", uri.c_str());
         int ret = proxy_->ConnectAbilityCommon(want, connect, callerToken, AppExecFwk::ExtensionAbilityType::DATASHARE);
         if (ret != ERR_OK) {
-            LOG_ERROR("connect ability failed, uri = ${public}s, ret = %{public}d", uri.c_str(), ret);
+            LOG_ERROR("connect ability failed, uri = %{public}s, ret = %{public}d", uri.c_str(), ret);
         }
         return ret;
     }
