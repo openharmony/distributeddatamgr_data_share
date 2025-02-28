8353d6c131131846250e30f8d098dfc05c3511ba
xuwenqi <xuwenqi6@h-partners.com>
2025-02-28 10:50:55 +0800

    想写的信息
    Signed-off-by: xuwenqi <xuwenqi6@h-partners.com>
    Change-Id: Idd8e736b58c72886dd6c9fb10728e8b99d8fb39f

--- test/native/unittest/mediadatashare_test/src/slientaccess_test.cpp
+++ test/native/unittest/mediadatashare_test/src/slientaccess_test.cpp
@@ -845,7 +845,7 @@ HWTEST_F(SlientAccessTest, SlientAccess_Creator_When_TimeOut_Test_001, TestSize.
 * @tc.desc: test UserDefineFunc
 * @tc.require: Null
 */
-HWTEST_F(SlientAccessTest, SlientAccess_UserDefineFunc_Test_002, TestSize.Level0)
+HWTEST_F(SlientAccessTest, SlientAccess_UserDefineFunc_Test_001, TestSize.Level0)
 {
     LOG_INFO("SilentAccess_UserDefineFunc_Test_001::Start");
     auto helper = g_slientAccessHelper;
