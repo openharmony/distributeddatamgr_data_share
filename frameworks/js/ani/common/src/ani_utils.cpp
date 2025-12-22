80dbdc530e93a37ce8a34bf698c82669978b98f8
tsatsulya <ermolaeva.varvara@huawei-partners.com>
2025-12-22 18:45:59 +0300

    [ani] correct signatures with any type
    Change-Id: I2c98885bea19546e4233caf3690285687430684a
    Signed-off-by: tsatsulya <ermolaeva.varvara@huawei-partners.com>

--- frameworks/js/ani/common/src/ani_utils.cpp
+++ frameworks/js/ani/common/src/ani_utils.cpp
@@ -283,7 +283,7 @@ bool UnionAccessor::TryConvertArray<bool>(std::vector<bool> &value)
 
     for (int i = 0; i < int(length); i++) {
         ani_ref ref;
-        if (ANI_OK != env_->Object_CallMethodByName_Ref(obj_, "$_get", "i:C{std.core.Object}", &ref, (ani_int)i)) {
+        if (ANI_OK != env_->Object_CallMethodByName_Ref(obj_, "$_get", "i:Y", &ref, (ani_int)i)) {
             LOG_ERROR("Object_GetPropertyByName_Ref failed");
             return false;
         }
@@ -316,7 +316,7 @@ bool UnionAccessor::TryConvertArray<int>(std::vector<int> &value)
 
     for (int i = 0; i < int(length); i++) {
         ani_ref ref;
-        if (ANI_OK != env_->Object_CallMethodByName_Ref(obj_, "$_get", "i:C{std.core.Object}", &ref, (ani_int)i)) {
+        if (ANI_OK != env_->Object_CallMethodByName_Ref(obj_, "$_get", "i:Y", &ref, (ani_int)i)) {
             LOG_ERROR("Object_GetPropertyByName_Ref failed");
             return false;
         }
@@ -349,7 +349,7 @@ bool UnionAccessor::TryConvertArray<double>(std::vector<double> &value)
 
     for (int i = 0; i < int(length); i++) {
         ani_ref ref;
-        if (ANI_OK != env_->Object_CallMethodByName_Ref(obj_, "$_get", "i:C{std.core.Object}", &ref, (ani_int)i)) {
+        if (ANI_OK != env_->Object_CallMethodByName_Ref(obj_, "$_get", "i:Y", &ref, (ani_int)i)) {
             LOG_ERROR("Object_GetPropertyByName_Ref failed");
             return false;
         }
@@ -407,7 +407,7 @@ bool UnionAccessor::TryConvertArray<std::string>(std::vector<std::string> &value
 
     for (int i = 0; i < int(length); i++) {
         ani_ref ref;
-        if (ANI_OK != env_->Object_CallMethodByName_Ref(obj_, "$_get", "i:C{std.core.Object}", &ref, (ani_int)i)) {
+        if (ANI_OK != env_->Object_CallMethodByName_Ref(obj_, "$_get", "i:Y", &ref, (ani_int)i)) {
             LOG_ERROR("Object_GetPropertyByName_Double length failed");
             return false;
         }
